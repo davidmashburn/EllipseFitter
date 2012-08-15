330286b43bc1204715a848ed1e2f9f4cc6d96665
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-15 00:24:45 -0500

    Created PyPI organizational structure

---
+++ README.txt
@@ -0,0 +1,1 @@
+EllipseFitter is a package from David Mashburn.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='EllipseFitter',
+    version='0.1',
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['EllipseFitter'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/EllipseFitter/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
